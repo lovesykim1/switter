16fb50af8b9ec2adf45984f94d115aff10e5277f
lovesykim1 <ang11585@naver.com>
2022-06-17 22:20:06 +0900

    1.0 Initializaion

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
-import AppRouter from "./Router";
+import { useEffect, useState } from "react";
+import AppRouter from "components/Router";
+import {authService} from "fbase";
 
 function App() {
-  return <AppRouter/>
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  useEffect(()=> {
+    authService.onAuthStateChanged((user) => console.log(user));
+  }, []);
+  return (
+    <>
+    <AppRouter isLoggedIn={isLoggedIn}/>
+    <footer>&copy; {new Date().getFullYear()} Switter</footer>
+    </>
+  ); 
 }
 
 export default App;
